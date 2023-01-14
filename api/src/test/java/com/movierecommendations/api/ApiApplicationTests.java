b3ae1f5798ec9a22daf8b1f15f74da6ae2ab7a06
ManelAC <53198006+ManelAC@users.noreply.github.com>
2023-01-14 21:09:01 +0100

    API inital setup
    Initial setup for the API

---
+++ api/src/test/java/com/movierecommendations/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movierecommendations.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
